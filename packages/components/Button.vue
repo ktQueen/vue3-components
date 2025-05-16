dbb03cd83389b632d0ec2fba0edf6854cbe84799
康婷 <kangting1@xiaomi.com>
2025-05-16 16:19:14 +0800

    feat： button组件

--- packages/components/Button.vue
+++ packages/components/Button.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="my-button">
-    <button>点击</button>
+    <button>
+      <!-- v-bind指令来传递所有未被Button组件明确声明的props到内部的Icon组件 -->
+      <my-icon v-bind="$attrs" />
+      {{ name }}
+    </button>
   </div>
 </template>
 
+<script setup lang="ts">
+defineProps<{
+  name: string;
+}>();
+
+// 阻止attrs继续传递到根元素
+defineOptions({
+  inheritAttrs: false,
+});
+</script>
 <style scoped lang="scss">
 .my-button {
   button {
     height: 32px;
     padding: 0 12px;
+    display: flex;
+    align-items: center;
   }
 }
 </style>
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <my-icon icon-name="icon-a-014-books" @click="handleClick" />
-    <my-button>点击</my-button>
+    <my-button name="新增" icon-name="icon-a-014-books" />
   </div>
 </template>
 <script setup lang="ts">
